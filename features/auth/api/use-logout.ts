658e5e07d05af0133ab184c2e1f66f9ba379bb89
patrick <patrick@patrickdeMacBook-Pro.local>
2024-11-25 16:38:25 +0800

    fix(hook): add toast after each fetch

--- features/auth/api/use-logout.ts
+++ features/auth/api/use-logout.ts
@@ -1,6 +1,8 @@
 import { client } from "@/lib/rpc";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
+import { useTranslations } from "next-intl";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<
   (typeof client)["api"]["auth"]["logout"]["$post"]
@@ -11,14 +13,30 @@ type RequestType = InferRequestType<
 
 export const useLogout = () => {
   const queryClient = useQueryClient();
+  const tt = useTranslations("toast");
+
+  let loadingId: string | number = "";
+
   const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
+      loadingId = toast.loading(tt("loading.logout"));
       const response = await client["api"]["auth"]["logout"]["$post"]();
       return await response.json();
     },
-    onSuccess: () => {
-      window.location.reload();
-      queryClient.invalidateQueries({ queryKey: ["current"] });
+    onSuccess() {
+      toast.success(tt("success.logout"), {
+        duration: 1500,
+        onAutoClose() {
+          window.location.reload();
+          queryClient.invalidateQueries({ queryKey: ["current"] });
+        },
+      });
+    },
+    onError() {
+      toast.error(tt("error.logout"));
+    },
+    onSettled() {
+      toast.dismiss(loadingId);
     },
   });
   return mutation;
--- features/auth/api/use-register.ts
+++ features/auth/api/use-register.ts
@@ -2,6 +2,8 @@ import { useRouter } from "@/i18n/routing";
 import { client } from "@/lib/rpc";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
+import { useTranslations } from "next-intl";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<
   (typeof client)["api"]["auth"]["register"]["$post"]
@@ -13,16 +15,32 @@ type RequestType = InferRequestType<
 export const useRegister = () => {
   const router = useRouter();
   const queryClient = useQueryClient();
+  const tt = useTranslations("toast");
+
+  let loadingId: string | number = "";
+
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
+      loadingId = toast.loading(tt("loading.login"));
       const response = await client["api"]["auth"]["register"]["$post"]({
         json,
       });
       return await response.json();
     },
-    onSuccess: () => {
-      router.refresh();
-      queryClient.invalidateQueries({ queryKey: ["current"] });
+    onSuccess() {
+      toast.success(tt("success.register"), {
+        duration: 1500,
+        onAutoClose() {
+          router.refresh();
+          queryClient.invalidateQueries({ queryKey: ["current"] });
+        },
+      });
+    },
+    onError(error) {
+      toast.error(tt("error.register"));
+    },
+    onSettled() {
+      toast.dismiss(loadingId);
     },
   });
   return mutation;
