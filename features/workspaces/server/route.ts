f26995e6ac7939eb8c0907a4f8b9ae0099bf58b1
patrick <patrick@patrickdeMacBook-Pro.local>
2024-11-26 12:17:54 +0800

    feat(server): add member and invite code attribute

--- features/workspaces/server/route.ts
+++ features/workspaces/server/route.ts
@@ -1,21 +1,35 @@
 import { zValidator } from "@hono/zod-validator";
+import { ID, Query } from "node-appwrite";
 import { Hono } from "hono";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import {
   DATABASE_ID,
   IMAGES_BUCKET_ID,
+  MEMBERS_ID,
   WORKSPACES_ID,
 } from "@/constant/config";
-import { ID } from "node-appwrite";
 import { getCreateWorkspaceSchema } from "@/lib/schema";
+import { ERole } from "@/interface/role";
+import { generateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
     const databases = c.get("databases");
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal("userId", user.$id),
+    ]);
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
 
     const worksspaces = await databases.listDocuments(
       DATABASE_ID,
-      WORKSPACES_ID
+      WORKSPACES_ID,
+      [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)]
     );
 
     return c.json({ data: worksspaces });
@@ -39,6 +53,7 @@ const app = new Hono()
           ID.unique(),
           image
         );
+        console.log("file:", file);
 
         const arrayBuffer = await storage.getFilePreview(
           IMAGES_BUCKET_ID,
@@ -50,7 +65,7 @@ const app = new Hono()
         ).toString("base64")}`;
       }
 
-      const worksspace = await databases.createDocument(
+      const workspace = await databases.createDocument(
         DATABASE_ID,
         WORKSPACES_ID,
         ID.unique(),
@@ -58,9 +73,17 @@ const app = new Hono()
           name,
           userId: user.$id,
           imageUrl: uploadImageUrl,
+          inviteCode: generateInviteCode(),
         }
       );
-      return c.json({ data: worksspace });
+
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: ERole.ADMIN,
+      });
+
+      return c.json({ data: workspace });
     }
   );
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+/**
+ * @description Generate a random invite code
+ * @param length length of the invite code
+ * @returns
+ */
+export function generateInviteCode(length: number = 6) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
