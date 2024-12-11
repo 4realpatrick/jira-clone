291522b1b8e97ec79739e82e2930c164be0fc39a
patrick <patrick@patrickdeMacBook-Pro.local>
2024-12-11 11:32:31 +0800

    fix(cmp): add onlyCancelToExit prop for ResponsiveModal cmp

--- components/common/responsive-modal.tsx
+++ components/common/responsive-modal.tsx
@@ -1,21 +1,42 @@
 import { useMedia } from "react-use";
-import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogDescription,
+} from "@/components/ui/alert-dialog";
 import { Drawer, DrawerContent, DrawerTitle } from "@/components/ui/drawer";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { Dialog, DialogContent, DialogTitle } from "../ui/dialog";
 
 interface IResponsiveModalProps {
   children: React.ReactNode;
   open: boolean;
+  onlyCancelToExit: boolean;
   onOpenChange: (open: boolean) => void;
 }
 
 export const ResponsiveModal: React.FC<IResponsiveModalProps> = ({
   children,
   open,
+  onlyCancelToExit,
   onOpenChange,
 }) => {
   const isPC = useMedia("(min-width: 1024px)", true);
   if (isPC) {
-    return (
+    return onlyCancelToExit ? (
+      <AlertDialog open={open} onOpenChange={onOpenChange}>
+        <AlertDialogContent className="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh] gap-0">
+          <AlertDialogTitle>
+            <VisuallyHidden>x</VisuallyHidden>
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            <VisuallyHidden>x</VisuallyHidden>
+          </AlertDialogDescription>
+          {children}
+        </AlertDialogContent>
+      </AlertDialog>
+    ) : (
       <Dialog open={open} onOpenChange={onOpenChange}>
         <DialogContent className="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]">
           <DialogTitle className="hidden"></DialogTitle>
